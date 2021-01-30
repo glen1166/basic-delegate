4213dc3e1493618b2f016cd17e5612b30ab5c5dc
Glen <glen1166@outlook.com>
2021-01-30 23:35:06 +0800

    delegate

---
+++ typedelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+public class Program
+{
+    public delegate string Reverse(string s);
+
+    static string ReverseString(string s)
+    {
+        return new string(s.Reverse().ToArray());
+    }
+
+    static void Main(string[] args)
+    {
+        Reverse rev = ReverseString;
+
+        Console.WriteLine(rev("a string"));
+    }
+}
