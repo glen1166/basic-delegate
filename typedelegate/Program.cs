4e087806b9a626e3f1b452205ad319a84db0f0ee
Glen <glen1166@outlook.com>
2021-01-31 15:47:00 +0800

    event

--- typedelegate/Program.cs
+++ typedelegate/Program.cs
@@ -1,24 +1,68 @@
 ﻿using System;
-using System.Linq;
 
-public class Program
+namespace ConsoleApplication1
 {
-    static void  ReverseString(string s)
+    class Program
     {
-        var str= new string(s.Reverse().ToArray());
-	Console.WriteLine(str);
+        static void Main(string[] args)
+        {
+            Counter c = new Counter(new Random().Next(10));
+            c.ThresholdReached += c_ThresholdReached;
+
+            Console.WriteLine("press 'a' key to increase total");
+            while (Console.ReadKey(true).KeyChar == 'a')
+            {
+                Console.WriteLine("adding one");
+                c.Add(1);
+            }
+        }
+
+        static void c_ThresholdReached(object sender, ThresholdReachedEventArgs e)
+        {
+            Console.WriteLine("The threshold of {0} was reached at {1}.", e.Threshold,  e.TimeReached);
+            Environment.Exit(0);
+        }
     }
 
-    static void PrintString(string s)
+    class Counter
     {
-	Console.WriteLine(s);
+        private int threshold;
+        private int total;
+
+        public Counter(int passedThreshold)
+        {
+            threshold = passedThreshold;
+        }
+
+        public void Add(int x)
+        {
+            total += x;
+            if (total >= threshold)
+            {
+                ThresholdReachedEventArgs args = new ThresholdReachedEventArgs();
+                args.Threshold = threshold;
+                args.TimeReached = DateTime.Now;
+                OnThresholdReached(args);
+            }
+        }
+
+        protected virtual void OnThresholdReached(ThresholdReachedEventArgs e)
+        {
+            EventHandler<ThresholdReachedEventArgs> handler = ThresholdReached;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        public event EventHandler<ThresholdReachedEventArgs> ThresholdReached;
     }
 
-    static void Main(string[] args)
+    public class ThresholdReachedEventArgs : EventArgs
     {
-        Action<string> rev = ReverseString;
-	Action<string> pri = PrintString;
-	Action<string> comb = (Action<string>)Delegate.Combine(rev, pri);
-	comb("Hello World!");
+        public int Threshold { get; set; }
+        public DateTime TimeReached { get; set; }
     }
 }
+
+
