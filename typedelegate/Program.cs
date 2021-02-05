b79b70a73da95d939c6efe29b23ce51545733225
glen1166 <glen1166@outlook.com>
2021-02-05 12:27:57 +0800

    observer design pattern

--- typedelegate/Program.cs
+++ typedelegate/Program.cs
@@ -1,76 +1,13 @@
 ﻿using System;
-using System.ComponentModel;
-namespace eventproperty{
+namespace typedelegate{
 
 	class Program
 	{
 		static void Main(string[] args){
-			SampleControl control = new SampleControl();
-			control.LightWentOn += (obj, para)=>{
-				Console.WriteLine("light went on.");
-			};
-
-			control.LightWentOut += (obj, para)=>{
-				Console.WriteLine("light went out.");
-			};
-			
-			control.FireEvent();
-		}
-	}
-
-	public	class SampleControl : Component
-	{
-
-		protected EventHandlerList listEventDelegates = new EventHandlerList();
-
-		// Define a unique key for each event.
-		static readonly object lightWentOn = new object();
-		static readonly object lightWentOut = new object();
-
-		public event EventHandler LightWentOn
-		{
-			// Add the input delegate to the collection.
-			add
-			{
-				listEventDelegates.AddHandler(lightWentOn, value);
-			}
-			// Remove the input delegate from the collection.
-			remove
-			{
-				listEventDelegates.RemoveHandler(lightWentOn, value);
-			}
-		}
-
-		private void OnLightWentOn(EventArgs e)
-		{
-			EventHandler lightWentOnDelegate = (EventHandler)listEventDelegates[lightWentOn];
-			lightWentOnDelegate(this, e);
-		}
-
-		public event EventHandler LightWentOut
-		{
-			// Add the input delegate to the collection.
-			add
-			{
-				listEventDelegates.AddHandler(lightWentOut, value);
-			}
-			// Remove the input delegate from the collection.
-			remove
-			{
-				listEventDelegates.RemoveHandler(lightWentOut, value);
-			}
-		}
-
-		private void OnLightWentOut(EventArgs e)
-		{
-			EventHandler lightWentOutDelegate = (EventHandler)listEventDelegates[lightWentOut];
-			lightWentOutDelegate(this, e);
-		}
-
-		public void FireEvent(){
-			var arg = new EventArgs();
-			OnLightWentOn(arg);
-			OnLightWentOut(arg);
+            var provider =	new TemperatureMonitor();
+            var observer = new TemperatureReporter();
+            observer.Subscribe(provider);
+            provider.GetTemperature();
 		}
 	}
 }
---
+++ typedelegate/Temperature.cs
@@ -0,0 +1,20 @@
+using System;
+namespace typedelegate{
+    public struct Temperature
+    {
+        private decimal temp;
+        private DateTime tempDate;
+
+        public Temperature(decimal temperature, DateTime dateAndTime)
+        {
+            this.temp = temperature;
+            this.tempDate = dateAndTime;
+        }
+
+        public decimal Degrees
+        { get { return this.temp; } }
+
+        public DateTime Date
+        { get { return this.tempDate; } }
+    }
+}
